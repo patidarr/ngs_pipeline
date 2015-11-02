b35d146f523707b1087015014063ccb8b87402aa
Rajesh Patidar <rajbtpatidar@gmail.com>
2015-11-02 13:54:47 -0500

    CoveragePlot Log Scale

--- scripts/coverage.R
+++ scripts/coverage.R
@@ -27,26 +27,28 @@ png(FILE, h=1000, w=1000, pointsize=20)
 # Create plot area, but do not plot anything. Add gridlines and axis labels.
 par(las=2)
 plot(
-	cov[[1]][2:401, 2], 
-	cov_cumul[[1]][1:400], 
-	log=cov[[1]][2:401, 2],
+	c(1,1000),
+	c(0,1.001),
+	log="x",
 	type='n', 
-	xaxs="i",
-	yaxs="i",
+	xaxt="n",
+	yaxt="n",
 	xlab="Coverage", 
 	ylab="Fraction of capture target bases \u2265 depth", 
 	ylim=c(0,1.001),
 	las=1,
-	xlim=c(-2,400),
+	xlim=c(1,1000),
 	main=paste(SAM, "\nTarget Region Coverage", sep="\t")
 	)
-abline(v =c(20,50, 80, 100, 200), col = "darkgray")
-abline(h =c(0.50, 0.80, 0.90), col = "darkgray")
+axis(1, at=c(5,10,20,50, 100, 200, 500, 1000))
+axis(2, at=c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1))
+abline(  v=c(5,10,20,50, 100, 200, 500, 1000), col = "darkgray")
+abline(  h=c(0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1), col="darkgray")
 
 # Actually plot the data for each of the alignments (stored in the lists).
 for (i in 1:length(cov)){
 	points(
-		cov[[i]][2:401, 2], cov_cumul[[i]][1:400], type='l', lwd=3, col=cols[i]
+		cov[[i]][2:1001, 2], cov_cumul[[i]][1:1000], type='l', lwd=3, col=cols[i]
 	)
 }
 box()
