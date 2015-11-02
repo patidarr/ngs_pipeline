8ba4e8445c06e1eb9c4acf4d44e92c462ff3fbb3
Rajesh Patidar <rajbtpatidar@gmail.com>
2015-11-02 11:44:42 -0500

    Fixed issues with coverageplot

--- scripts/coverage.R
+++ scripts/coverage.R
@@ -2,15 +2,18 @@ library(stringr)
 args<-commandArgs()
 print(args[6])
 print(args[7])
+print(args[8])
 DIR = str_trim(args[6])
 FILE=str_trim(args[7])
+SAM=str_trim(args[8])
+
 
 files <- list.files(path = DIR, pattern="coverage.txt$")
 
 cov <- list()
 cov_cumul <- list()
 for (i in 1:length(files)) {
-    cov[[i]] <- read.table(files[i])
+    cov[[i]] <- read.table(paste(DIR,"/",files[i], sep=""))
     cov_cumul[[i]] <- 1-cumsum(cov[[i]][,5])
 }
 labs <- paste("", gsub("Sample_|\\.coverage\\.txt", "", files, perl=TRUE), sep="")
@@ -26,6 +29,7 @@ par(las=2)
 plot(
 	cov[[1]][2:401, 2], 
 	cov_cumul[[1]][1:400], 
+	log=cov[[1]][2:401, 2],
 	type='n', 
 	xaxs="i",
 	yaxs="i",
@@ -34,7 +38,7 @@ plot(
 	ylim=c(0,1.001),
 	las=1,
 	xlim=c(-2,400),
-	main="Target Region Coverage"
+	main=paste(SAM, "\nTarget Region Coverage", sep="\t")
 	)
 abline(v =c(20,50, 80, 100, 200), col = "darkgray")
 abline(h =c(0.50, 0.80, 0.90), col = "darkgray")
