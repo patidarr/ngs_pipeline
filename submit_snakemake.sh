1909816e769d1d115a92380677bf2875872977c8
Rajesh Patidar <rajbtpatidar@gmail.com>
2015-11-17 17:00:04 -0500

    Major changes in directory structure

--- submit_snakemake.sh
+++ submit_snakemake.sh
@@ -35,7 +35,7 @@ snakemake\
 	-j 3000 \
 	--stats ngs_pipeline_${NOW}.stats \
 	--cluster "sbatch -o log/{params.rulename}.%j.o {params.batch}"\
-	>& serpentine_${NOW}.log
+	>& ngs_pipeline_${NOW}.log
 
 # Summary 
 #  snakemake --directory $WORK_DIR --snakefile $SNAKEFILE --configfile $SAM_CONFIG --summary
