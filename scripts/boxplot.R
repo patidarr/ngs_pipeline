bda7816d3d066330047a443329391ee4e11d17f1
Rajesh Patidar <rajbtpatidar@gmail.com>
2015-11-19 08:48:25 -0500

    change Y max in boxplot

--- scripts/boxplot.R
+++ scripts/boxplot.R
@@ -24,7 +24,7 @@ for (i in 1:length(files)){
 
 png(FILE,width = 1000, height = 1000, res=100);
 par(mar = c(18, 5, 4, 2) + 0.1)
-plot(10,10,  xlim=c(1,length(files)+1), ylim=c(0,y_max+100), xaxt='n', yaxt="n", xlab='', ylab='Coverage', main=paste(SAM, "Hotspot Coverage", sep="\n"))
+plot(10,10,  xlim=c(1,length(files)+1), ylim=c(0,y_max+1), xaxt='n', yaxt="n", xlab='', ylab='Coverage', main=paste(SAM, "Hotspot Coverage", sep="\n"))
 
 for (i in 1:length(files)){
         cov.data   <-read.table(paste(DIR,files[i] ,sep = ""), sep="\t", quote="", head=F);
