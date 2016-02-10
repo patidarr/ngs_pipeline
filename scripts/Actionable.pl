b17b06c9f0055d570a1ff7ab410301b0d97f5a3a
Rajesh Patidar <rajbtpatidar@gmail.com>
2016-02-10 11:29:36 -0500

    Added cancergenecensus to somatic actionable

--- scripts/Actionable.pl
+++ scripts/Actionable.pl
@@ -3,14 +3,16 @@ use strict;
 use warnings;
 
 if($ARGV[0] eq 'somatic'){
-	Somatic($ARGV[1], $ARGV[2]);
+	Somatic($ARGV[1], $ARGV[2], $ARGV[3], $ARGV[4]);
 	# 1 == HotspotFile [reference]
-	# 2 == somaticFile
+	# 2 == CancerGene Census [reference]
+	# 3 == somaticFile
+	# 4 == Annotations
 }
 elsif($ARGV[0] eq 'germline'){
 	Germline($ARGV[1], $ARGV[2], $ARGV[3], $ARGV[4], $ARGV[5]);
 	# 1 == somaticFile
-	# 2 == germlineFil
+	# 2 == germlineFile
 	# 3 == Annotations
 	# 4 == CancerGene List [reference]
 	# 5 == HotspotFile [reference]
@@ -104,9 +106,10 @@ sub Germline{
 							$source = $source.";".$HOT_SPOT{"$temp[0]\t$temp[1]\t$temp[2]"};
 						}
 					}
-					if(exists $HOT_SPOT{"$temp[0]\t$temp[1]\t$temp[2]"}){
-						$source = $source.";".$HOT_SPOT{"$temp[0]\t$temp[1]\t$temp[2]"};
-					
+					else{
+						if (exists $HOT_SPOT{"$temp[0]\t$temp[1]\t$temp[2]"}){
+							$source = $source.";".$HOT_SPOT{"$temp[0]\t$temp[1]\t$temp[2]"};
+						}	
 					}
 					print "$temp[0]\t$temp[1]\t$temp[2]\t$temp[3]\t$temp[4]\t$ANNOTATION{$site}\t$vcf\t$vaf\t$source\n";
 				}
@@ -136,7 +139,7 @@ sub findSource{
 }
 sub Somatic{
 #/data/Clinomics/Ref/annovar/hg19_SomaticActionableSites.txt NCI0276/NCI0276/db/NCI0276.somatic
-	my ($ref, $subject) = (@_);
+	my ($ref, $cgc ,$subject, $annotation) = (@_);
 	unless (open(ANN_FH, "$ref")){
 		print STDERR "\n\nCan not open $ref\n"; 
 		exit;
@@ -151,11 +154,39 @@ sub Somatic{
 		$DATA{"$key"} = $value;
 	}
 	close ANN_FH;
+	unless (open(ANN_FH, "$cgc")){
+		print STDERR "\n\nCan not open $ref\n";
+		exit;
+	}
+	my %CGC;
+	while(<ANN_FH>){
+		chomp;
+		my @local = split ("\t", $_);
+		$CGC{"$local[0]"} = "CancerGeneCensus";
+	}
+	close ANN_FH;
 	unless (open (ORI,"$subject")){
 		print STDERR "\n\nCan not open $subject\n"; 
 		exit;
 	}
-	print "Chr\tStart\tEnd\tRef\tAlt\tSample\tCaller\tQUAL\tFS\tTotalReads\tAltReads\tVAF\tSource\n";
+	#Annotations to hash
+	unless (open(REF, "$annotation")){
+		print STDERR "Can not open file $annotation\n";
+		exit;
+	}
+	my %ANNOTATION;
+	while(<REF>){
+		chomp;
+		my @local = split ("\t", $_);
+		my $key = join "\t", @local[0..4];
+		my $end = @local - 1 ;
+		my $value = join "\t", @local[5..$end];
+		$ANNOTATION{$key} =$value;
+	}
+	close REF;
+	print "Chr\tStart\tEnd\tRef\tAlt\t";
+	print $ANNOTATION{"Chr\tStart\tEnd\tRef\tAlt"};
+	print "\tSample\tCaller\tQUAL\tFS\tTotalReads\tAltReads\tVAF\tSource\n";
 	while (<ORI>){
 		chomp;
 		my @temp = split("\t", $_);
@@ -163,10 +194,17 @@ sub Somatic{
 		my $vcf;	
 		my $end = @temp - 1 ;
 		my $vaf = VAF($temp[9], $temp[10]);
+		my $key = join "\t", @temp[0..4];
 		$val = "$temp[0]\t$temp[1]\t$temp[2]";
-		$vcf = join "\t", @temp[3..$end];
+		$vcf = join "\t", @temp[5..$end];
 		if (exists $DATA{$val}){
-			print "$val\t$vcf\t$vaf\t$DATA{$val}\n";
+			print "$key\t$ANNOTATION{$key}\t$vcf\t$vaf\t$DATA{$val}\n";
+		}
+		else{
+			my @local = split("\t",$ANNOTATION{$key});
+			if (exists $CGC{$local[1]} and( $local[3] =~ /stopgain/ or $local[3]=~ /^frameshift/ or $local[0] =~ /splicing/)){
+				print "$key\t$ANNOTATION{$key}\t$vcf\t$vaf\t$CGC{$local[1]}\n";
+			}
 		}
 	}
 	close ORI;
