5fe40ef1819462d21b454f02c2e2736dca6a0da4
Rajesh Patidar <rajbtpatidar@gmail.com>
2016-04-01 16:44:32 -0400

    seperate config files for cluster also changed other settings based on host

--- submit_snakemake.sh
+++ submit_snakemake.sh
@@ -11,38 +11,52 @@
 NOW=$(date +"%Y%m%d_%H")
 module use /data/khanlab/apps/Modules
 module load python/3.4.3
-export NGS_PIPELINE="/data/khanlab/projects/patidar/Snakemake"
-export WORK_DIR="/data/khanlab/projects/patidar/Snakemake/test"
-export DATA_DIR="/data/khanlab/projects/DATA"
+module load snakemake
+if [ `hostname` == 'biowulf.nih.gov' ]; then
+	export NGS_PIPELINE="/data/khanlab/projects/patidar/Snakemake"
+	export WORK_DIR="/data/khanlab/projects/patidar/Snakemake/test"
+	export DATA_DIR="/data/khanlab/projects/DATA"
+	export ACT_DIR="/Actionable/"
+	export HOST="biowulf.nih.gov"
+
+elif [[ `hostname` =~ "tgcompute" ]] || [ `hostname` == 'login01' ] ; then
+	export NGS_PIPELINE="/home/patidarr/ngs_pipeline/"
+	export WORK_DIR="/home/patidarr/trial1/"
+	export DATA_DIR="/projects/Clinomics/DATA/"
+	export ACT_DIR="/Actionable/"
+	export HOST="login01"
+else 
+	echo "`hostname` is not recognized\n"
+	exit;
+fi
+
 SNAKEFILE=$NGS_PIPELINE/ngs_pipeline.rules
 SAM_CONFIG=$WORK_DIR/samplesheet.json
-ACT_DIR="/Actionable/"
-if [ `cat $SAM_CONFIG |/usr/bin/json_verify -c` -ne "JSON is valid" ]; then
-	echo "$SAM_CONFIG is not a valid json file"
-	exit
-fi
 
 cd $WORK_DIR
+#if [ `cat $SAM_CONFIG |/usr/bin/json_verify -c` -ne "JSON is valid" ]; then
+#       echo "$SAM_CONFIG is not a valid json file"
+#       exit
+#fi
 if [ ! -d log ]; then
-    mkdir log
+	mkdir log
 fi
-
-if [ ! -d annovar ]; then 
+if [ ! -d annovar ]; then
 	mkdir annovar
 	touch annovar/AnnotationInput.final.txt
 fi
 
-snakemake\
-	--directory $WORK_DIR \
-	--snakefile $SNAKEFILE \
-	--configfile $SAM_CONFIG \
-	--jobname '{params.rulename}.{jobid}' \
-	--nolock \
-	-k -p -T \
-	-j 3000 \
-	--stats ngs_pipeline_${NOW}.stats \
-	--cluster "sbatch --mail-type=FAIL -o log/{params.rulename}.%j.o {params.batch}"\
-	>& ngs_pipeline_${NOW}.log
+
+
+cmd="--directory $WORK_DIR --snakefile $SNAKEFILE --configfile $SAM_CONFIG --jobname '{params.rulename}.{jobid}' --nolock  -k -p -T -j 3000 --stats ngs_pipeline_${NOW}.stats"
+if [ $HOST   == 'biowulf.nih.gov' ]; then
+	echo "Host identified as $HOST"
+	snakemake $cmd --cluster "sbatch --mail-type=FAIL -o log/{params.rulename}.%j.o {params.batch}" >& ngs_pipeline_${NOW}.log
+elif [ $HOST == 'login01' ]; then
+	 echo "Host identified as $HOST"
+	snakemake $cmd --cluster "qsub  -V -o $WORK_DIR/log/ {params.batch}" >& ngs_pipeline_${NOW}.log
+fi
+
 
 # Summary 
 #  snakemake --directory $WORK_DIR --snakefile $SNAKEFILE --configfile $SAM_CONFIG --summary
