ec3f25ac77991ddf677973ed8aa8b83f4eb198c7
Rajesh Patidar <rajbtpatidar@gmail.com>
2016-04-04 10:34:27 -0400

    Minor fixes to accomodate TGen Host.

--- submit_snakemake.sh
+++ submit_snakemake.sh
@@ -1,37 +1,37 @@
 #!/bin/sh
 #SBATCH --job-name="KhanLab"
 #SBATCH --mail-type=FAIL
-#SBATCH --partition="unlimited"
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=1g
-# Make sure log directory exists or change the output file location on line 4.
-#
+#SBATCH --gres=lscratch:01
 #
 #NOW=$(date +"%H%M%S_%m%d%Y")
 NOW=$(date +"%Y%m%d_%H")
-module use /data/khanlab/apps/Modules
-module load python/3.4.3
-module load snakemake
 if [[ `hostname` =~ "cn" ]] || [ `hostname` == 'biowulf.nih.gov' ]; then
+	module use /data/khanlab/apps/Modules
+	module load python/3.4.3
 	export NGS_PIPELINE="/data/khanlab/projects/patidar/Snakemake"
 	export WORK_DIR="/data/khanlab/projects/patidar/Snakemake/test"
 	export DATA_DIR="/data/khanlab/projects/DATA"
 	export ACT_DIR="/Actionable/"
 	export HOST="biowulf.nih.gov"
-
+	SNAKEFILE=$NGS_PIPELINE/ngs_pipeline.rules
+	SAM_CONFIG=$WORK_DIR/samplesheet.json
 elif [[ `hostname` =~ "tgcompute" ]] || [ `hostname` == 'login01' ] ; then
+	module load python/3.4.3
+	module load snakemake
 	export NGS_PIPELINE="/home/patidarr/ngs_pipeline/"
 	export WORK_DIR="/home/patidarr/trial1/"
 	export DATA_DIR="/projects/Clinomics/DATA/"
 	export ACT_DIR="/Actionable/"
 	export HOST="login01"
+	SNAKEFILE=$NGS_PIPELINE/ngs_pipeline.rules
+	SAM_CONFIG=$WORK_DIR/samplesheet.json
 else 
-	echo "Host `hostname` is not recognized\n"
+	echo -e "Host `hostname` is not recognized\n"
+	echo -e "This pipeline is customized to run on biowulf.nih.gov or TGen Cluster@ KhanLab\n";
 	exit;
 fi
-
-SNAKEFILE=$NGS_PIPELINE/ngs_pipeline.rules
-SAM_CONFIG=$WORK_DIR/samplesheet.json
 
 cd $WORK_DIR
 #if [ `cat $SAM_CONFIG |/usr/bin/json_verify -c` -ne "JSON is valid" ]; then
@@ -70,5 +70,5 @@ fi
 #Rulegraph
 #  snakemake --directory $WORK_DIR --snakefile $SNAKEFILE --configfile $SAM_CONFIG -n --forceall --rulegraph | dot -Tpng > rulegraph.png
 
-# Mail Rulegraph and DAG to self
+# Mail Rulegraph and DAG to self#  echo DAG |mutt -s "DAG" -a dag.png -a rulegraph.png -- patidarr@mail.nih.gov
 #  echo DAG |mutt -s "DAG" -a dag.png -a rulegraph.png -- patidarr@mail.nih.gov
