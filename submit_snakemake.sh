88965ed41caf12443d6e4109881d236c497c81b8
patidarr <rajbtpatidar@gmail.com>
2016-05-02 19:47:23 -0400

    NGS_PIPELINE location coming from launch only

--- submit_snakemake.sh
+++ submit_snakemake.sh
@@ -14,34 +14,27 @@ else
 	echo -e "Can not run without knowing which mode you would like to set time up\n";
 	exit;
 fi
+if [[ ! -z $ngs ]]; then
+        export NGS_PIPELINE=$ngs
+fi
 #export TIME=$(date +"%Y%m%d")
 #export TIME=$(date +"%Y%m%d%H")
 if [[ `hostname` =~ "cn" ]] || [ `hostname` == 'biowulf.nih.gov' ]; then
 	module load python/3.4.3
 	module load snakemake/3.5.5.n1
-	export NGS_PIPELINE="/data/Clinomics/Tools/ngs_pipeline-dev/"
 	export WORK_DIR="/data/khanlab/projects/DNASeq/"
 	export DATA_DIR="/data/khanlab/projects/DATA/"
-	export ACT_DIR="/Actionable/"
 	export HOST="biowulf.nih.gov"
-	SNAKEFILE=$NGS_PIPELINE/ngs_pipeline.snakefile
-	SAM_CONFIG=$WORK_DIR/samplesheet.json
-	
 elif [[ `hostname` =~ "tghighmem" ]] || [[ `hostname` =~ "tgcompute" ]] || [ `hostname` == 'login01' ] ; then
-	#module use /home/patidarr/Modules
 	module load python/3.4.3
 	module load snakemake/3.5.5
-	export NGS_PIPELINE="/projects/Clinomics/Tools/ngs_pipeline/"
-	export WORK_DIR="/projects/Clinomics/Test_Run3/"
+	export WORK_DIR="/projects/Clinomics/Test_Run2/"
 	export DATA_DIR="/projects/Clinomics/DATA/"
-	export ACT_DIR="/Actionable/"
 	export HOST="login01"
-	SNAKEFILE=$NGS_PIPELINE/ngs_pipeline.snakefile
-	SAM_CONFIG=$WORK_DIR/samplesheet.json
 else 
 	echo -e "Host `hostname` is not recognized\n"
 	echo -e "This pipeline is customized to run on biowulf.nih.gov or TGen Cluster @ KhanLab\n";
-	echo -e "If you would like to use it on another system, you have to change config/config_cluster.json and some hardcoded system dependent\n";
+	echo -e "If you would like to use it on another system, you have to change config/config_cluster.json and some hardcoded system dependencies\n";
 	exit;
 fi
 
@@ -63,6 +56,11 @@ if [ ! -d annovar ]; then
 	mkdir annovar
 	touch annovar/AnnotationInput.final.txt
 fi
+
+export ACT_DIR="/Actionable/"
+SNAKEFILE=$NGS_PIPELINE/ngs_pipeline.snakefile
+SAM_CONFIG=$WORK_DIR/samplesheet.json
+
 cmd="--directory $WORK_DIR --snakefile $SNAKEFILE --configfile $SAM_CONFIG --jobname {params.rulename}.{jobid} --nolock  -k -p -T -j 3000 --stats ngs_pipeline_${TIME}.stats"
 if [ $HOST   == 'biowulf.nih.gov' ]; then
 	echo "Host identified as $HOST"
@@ -72,17 +70,3 @@ elif [ $HOST == 'login01' ]; then
 	snakemake $cmd --cluster "qsub -W umask=022 -V -e $WORK_DIR/log/ -o $WORK_DIR/log/ {params.batch}" >& ngs_pipeline_${TIME}.log
 fi
 chmod -R 777 $WORK_DIR/.snakemake
-# Summary 
-#  snakemake --directory $WORK_DIR --snakefile $SNAKEFILE --configfile $SAM_CONFIG --summary
-
-## DRY Run with Print out the shell commands that will be executed
-#  snakemake --directory $WORK_DIR --snakefile $SNAKEFILE --configfile $SAM_CONFIG --dryrun -p -r
-
-#DAG 
-#  snakemake --directory $WORK_DIR --snakefile $SNAKEFILE --configfile $SAM_CONFIG --dag | dot -Tpng > dag.png
-
-#Rulegraph
-#  snakemake --directory $WORK_DIR --snakefile $SNAKEFILE --configfile $SAM_CONFIG -n --forceall --rulegraph | dot -Tpng > Rulegraph.png
-
-# Mail Rulegraph and DAG to self#  echo DAG |mutt -s "DAG" -a dag.png -a rulegraph.png -- patidarr@mail.nih.gov
-#  echo DAG |mutt -s "DAG" -a dag.png -a Rulegraph.png -- patidarr@mail.nih.gov
