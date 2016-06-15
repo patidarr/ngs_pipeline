7333883c5a3bfae1c2bf649c7949029e5bff85f7
patidarr <rajbtpatidar@gmail.com>
2016-06-15 16:47:19 -0400

    This is to replace use of mpileup, will use HC_RNASeq calls to get expressed somatic mutations

---
+++ scripts/ExpressedMutations.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+#########################
+# Author: Rajesh Patidar rajbtpatidar@gmail.com
+#
+# This script takes a formatted vcf file (tdf) (Somatic) and calls on RNASeq of the same tumor and try to find
+#  if the somatic indels are present in the RNASeq or not (Expressed).
+#
+#########################
+my $SOMATIC = $ARGV[0];
+my $RNA     = $ARGV[1];
+my $RNAname = $ARGV[2];
+open (IN1, $SOMATIC);
+open(IN2, $RNA);
+
+
+my %Expressed;
+while(<IN2>){
+	chomp;
+	my @line = split("\t",$_);
+	$Expressed{join("\t",@line[0..4])} = join("\t",@line[9..13]);
+}
+foreach (sort keys %Expressed) {
+#	print "$_ : $Expressed{$_}\n";
+}
+
+while(<IN1>){
+	chomp;
+	if ($_ =~ /^Chr/){
+		print "$_\t$RNAname.GT\tRNASeq.TotCov\tRNASeq.RefCov\tRNASeq.VarCov\tRNASeq.VAF\n";
+	}
+	else{
+		my @line = split("\t",$_);
+		if (exists $Expressed{join("\t",@line[0..4])}){
+			print "$_\t".$Expressed{join("\t",@line[0..4])}."\n";
+		}
+		else{
+			my ($status,$info) = search($line[0], $line[1], $line[2]);
+			if ($status eq 'match'){
+				print "$_\t$info\n";
+			}
+			else{
+				print "$_\tNA\tNA\tNA\tNA\tNA\n";
+					
+			}
+		}
+	}
+}
+
+sub search{
+	my ($chr, $start, $end) = (@_);
+	foreach my $indel (keys %Expressed){
+		my @ind = split("\t", $indel);
+		if ($ind[0] eq $chr and ($start ~~ [$ind[1] - 10 .. $ind[1] + 10]  or $end ~~ [$ind[2] - 10 .. $ind[2] + 10])){
+			return("match", $Expressed{$indel});
+		}
+	}
+}
