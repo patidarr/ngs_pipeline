42c7ce6804ca18ad2b32d6313845838163e27331
patidarr <rajbtpatidar@gmail.com>
2016-06-22 09:43:56 -0400

    script to make unique somatic mutation/library.

---
+++ scripts/UnionSomaticCalls.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use File::Basename;
+use List::Util qw(first);
+#
+# Author Rajesh Patidar rajbtpatidar@gmail.com
+#
+#
+# Strelka Expressed or Strelka (SNPS)
+# Strelka Expressed or Strelka (Indels)
+# muTect expressed or MuTect
+
+my %CALLS;
+
+foreach my $file(@ARGV){
+	unless (open(FH, $file)){
+		print STDERR "Can not open the file $file\n";
+		exit;
+	}
+	my @a = split(/[.]/, basename($file));
+	while(<FH>){
+		chomp;
+		my @line = split("\t", $_);
+		my $key  = "$line[0]\t$line[1]\t$line[2]\t$line[3]\t$line[4]";
+		if ($_ =~ /^Chr/){
+			$CALLS{$key} = "$_\tCaller";
+			next
+		}
+		my $key  = "$line[0]\t$line[1]\t$line[2]\t$line[3]\t$line[4]";
+		$CALLS{$key} = "$_\t$a[1]";
+	}
+	close FH;
+}
+foreach (sort keys %CALLS) {
+    print "$CALLS{$_}\n";
+}
