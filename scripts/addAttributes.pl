10fea94ddf17410e68260f93a09cd8335642dbc8
patidarr <rajbtpatidar@gmail.com>
2016-07-26 12:46:44 -0400

    Round the numbers

--- scripts/addAttributes.pl
+++ scripts/addAttributes.pl
@@ -8,6 +8,9 @@ local $SIG{__WARN__} = sub {
 };
 my $hs      =`/bin/grep \"^$ARGV[0]\" $ARGV[1] |cut -f 21,22`;
 chomp $hs;
+my @cov =split("\t", $hs);
+my $hs1  =sprintf("%.f", $cov[0]);
+my $hs2 +=sprintf("%.f", $cov[1]);
 my $in_file =$ARGV[2];
 my $out_file=$ARGV[3];
 open (IN, "<$in_file") or die "Can't open $in_file: $!\n";
@@ -22,7 +25,7 @@ while(<IN>){
 		print OUT join("\t", @line[0..2]). "\tMEAN_BAIT_COVERAGE\tMEAN_TARGET_COVERAGE\t". join("\t", @line[3..$#line])."\n";
 	}
 	else{
-		print OUT join("\t", @line[0..2]). "\t$hs\t". join("\t", @line[3..$#line])."\n";
+		print OUT join("\t", @line[0..2]). "\t$hs1\t$hs2\t". join("\t", @line[3..$#line])."\n";
 	}
 }
 close IN;
