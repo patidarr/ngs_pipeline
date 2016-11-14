6e1bc424b062c56f3e4ee7576d10b0ed25243ff6
patidarr <rajbtpatidar@gmail.com>
2016-11-14 10:01:11 -0500

    remove nonframeshift from LOF

--- scripts/Actionable.v3.pl
+++ scripts/Actionable.v3.pl
@@ -173,7 +173,7 @@ sub Germline{
                                         elsif ($ANN[$index_of_HGMD] =~ /^Disease causing mutation$/){
                                                 $level{"1.2"} = "yes";
                                         }
-					elsif ($ANN[$idx_anno_region] =~ /splicing/ or $ANN[$idx_anno_eff] =~ /stopgain/ or $ANN[$idx_anno_eff]=~ /^frameshift/ or $ANN[$idx_anno_eff]=~ /nonframeshift deletion/ or $ANN[$idx_anno_eff]=~ /nonframeshift insertion/ or ($ANN[$idx_anno_eff]=~ /nonframeshift substitution/ and length($temp[3]) <=3 )){
+					elsif ($ANN[$idx_anno_region] =~ /splicing/ or $ANN[$idx_anno_eff] =~ /stopgain/ or $ANN[$idx_anno_eff]=~ /^frameshift/)){
                                                 $level{"1"} = "yes";
                                         }
                                         else{
@@ -187,7 +187,7 @@ sub Germline{
 					elsif ($ANN[$index_of_HGMD] =~ /^Disease causing mutation$/){
                                                 $level{"1.2"} = "yes";
                                         }
-					if ($ANN[$idx_anno_region] =~ /splicing/ or $ANN[$idx_anno_eff] =~ /stopgain/ or $ANN[$idx_anno_eff]=~ /^frameshift/ or $ANN[$idx_anno_eff]=~ /nonframeshift deletion/ or $ANN[$idx_anno_eff]=~ /nonframeshift insertion/ or ($ANN[$idx_anno_eff]=~ /nonframeshift substitution/ and length($temp[3]) <=3 )){
+					if ($ANN[$idx_anno_region] =~ /splicing/ or $ANN[$idx_anno_eff] =~ /stopgain/ or $ANN[$idx_anno_eff]=~ /^frameshift/)){
 						$level{"1"} = "yes";
 					}
 					else{
